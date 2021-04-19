53a30550117c1644fd68364b4e5e2f89821e16ec
Rkrawec <rkrawec@umich.edu>
2021-04-19 17:18:01 -0400

    Update mean shift

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import cv2
 import time
 from copy import deepcopy
 
-cap = cv2.VideoCapture('vid.mp4')
+
+NUM_ITERAIONS = 2
+
+cap = cv2.VideoCapture('vid2.mp4')
 
 # take first frame of the video
 ret, frame = cap.read()
@@ -29,8 +32,66 @@ cv2.imshow('mask', mask)
 roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
 cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+# Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+
+# #input track_window
+# #output np array of points representing bounding box
+#This might be wrong
+def makeBoundingBox(track_window):
+    return np.array([[track_window[0], track_window[1]],
+                     [track_window[0]+ track_window[2], track_window[1]],
+                     [track_window[0], track_window[1] + track_window[3]],
+                     [track_window[0]+ track_window[2], track_window[1] + track_window[3]]])
+
+
+# using the bounding, choose a circle of region of interest
+# use the circle of interest as indexing into dst for points in cicle
+# find center of mass of dst indexed by the circel of interest
+# shift bounding box to be centered on mean
+def calculatePoints(trackWindow, radiusCircleInterest, dst):
+    c,r,w,h = trackWindow[0],trackWindow[1] ,trackWindow[2] ,trackWindow[3]
+    center = (c + w/2, r + h/2)
+    boundingBox = makeBoundingBox(track_window)
+
+    #keep these as floats or integer overflow
+    massCenterRow = 0.0
+    massCenterCol = 0.0
+    totalIntensity = 0.0
+    #out = []
+
+    # for col in range(c,c+w):
+    #     for row in range(r,r+h):
+    for col in range(int(c + w/4),int(c+w - w/4)):
+        for row in range(int(r + h/4),int(r+h - h/4)):
+            #out.append((row,col,dst[row][col]))
+            if row >= len(dst):
+                row = len(dst)-1
+            elif col >= len(dst[0]):
+                col = len(dst[0])-1
+            massCenterRow += dst[row][col] * row
+            massCenterCol += dst[row][col] * col
+            totalIntensity += dst[row][col]
+    massCenterRow /= totalIntensity
+    massCenterCol /= totalIntensity
+
+    massCenterRow = int(massCenterRow)
+    massCenterCol = int(massCenterCol)
+
+
+    pts = np.array([[massCenterCol-h/2, massCenterRow-w/2],
+                    [massCenterCol-h/2, massCenterRow+w/2],
+                    [massCenterCol+h/2, massCenterRow+w/2],
+                    [massCenterCol+h/2, massCenterRow-w/2]])
+
+    return pts
+
+# for x, y in dst:
+# if rad^2 >= (x-center[0])^2 + (y - center[1])^2:
+#   add point to be counted in calculating the center of mass (r, c, intensity)
+
+
 
 while True:
     # time.sleep(1)
@@ -41,12 +102,17 @@ while True:
         dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
         # apply meanshift to get the new location
-        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+        # ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+
+        pts = 0
+        for i in range(NUM_ITERAIONS):
+            pts = calculatePoints(track_window, 10, dst)
 
         # Draw it on image
-        pts = cv2.boxPoints(ret)
-        pts = np.int0(pts)
-        img2 = cv2.polylines(frame, [pts], True, 255, 2)
+        # pts = cv2.boxPoints(ret)
+        # pts = np.int0(pts)
+        #pts = np.array([[10, 10], [800, 10], [800, 400], [10, 400]])
+        img2 = cv2.polylines(frame, np.int32([pts]), True, 255, 2)
         cv2.imshow('img2', img2)
 
         k = cv2.waitKey(60) & 0xff
