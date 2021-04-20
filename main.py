f705d28782588da1210f9d6355956ef2472000b7
Rkrawec <rkrawec@umich.edu>
2021-04-19 20:28:12 -0400

    Fix error where box does not move

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 import time
+import math
 from copy import deepcopy
 
 
-NUM_ITERAIONS = 2
+NUM_ITERAIONS = 10
 
-cap = cv2.VideoCapture('vid2.mp4')
+# cap = cv2.VideoCapture('vid2.mp4')
+cap = cv2.VideoCapture(0)
 
 # take first frame of the video
 ret, frame = cap.read()
@@ -15,6 +17,11 @@ ret, frame = cap.read()
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 face = detector.detectMultiScale(gray, 1.1, 4)
+while len(face) == 0:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face = detector.detectMultiScale(gray, 1.1, 4)
 # r,h,c,w = 250,90,400,125  # simply hardcoded the values
 # print(face)
 (c, r, w, h) = face[0]
@@ -53,7 +60,7 @@ def makeBoundingBox(track_window):
 def calculatePoints(trackWindow, radiusCircleInterest, dst):
     c,r,w,h = trackWindow[0],trackWindow[1] ,trackWindow[2] ,trackWindow[3]
     center = (c + w/2, r + h/2)
-    boundingBox = makeBoundingBox(track_window)
+    #boundingBox = makeBoundingBox(track_window)
 
     #keep these as floats or integer overflow
     massCenterRow = 0.0
@@ -63,29 +70,50 @@ def calculatePoints(trackWindow, radiusCircleInterest, dst):
 
     # for col in range(c,c+w):
     #     for row in range(r,r+h):
-    for col in range(int(c + w/4),int(c+w - w/4)):
-        for row in range(int(r + h/4),int(r+h - h/4)):
-            #out.append((row,col,dst[row][col]))
-            if row >= len(dst):
-                row = len(dst)-1
-            elif col >= len(dst[0]):
-                col = len(dst[0])-1
-            massCenterRow += dst[row][col] * row
-            massCenterCol += dst[row][col] * col
-            totalIntensity += dst[row][col]
-    massCenterRow /= totalIntensity
-    massCenterCol /= totalIntensity
+    #
+    # # for col in range(int(c + w/4),int(c+w - w/4)):
+    # #     for row in range(int(r + h/4),int(r+h - h/4)):
+    #         #out.append((row,col,dst[row][col]))
+    #         if row >= len(dst):
+    #             row = len(dst)-1
+    #         elif col >= len(dst[0]):
+    #             col = len(dst[0])-1
+    #         massCenterRow += dst[row][col] * row
+    #         massCenterCol += dst[row][col] * col
+    #         totalIntensity += dst[row][col]
+    # massCenterRow /= totalIntensity
+    # massCenterCol /= totalIntensity
+
+
+
+    intensities = dst[int(r + h/4):int(r+h - h/4),int(c + w/4):int(c+w - w/4)]
+    grid = np.indices((dst.shape[0], dst.shape[1]))
+    grid = grid[:,int(r + h/4):int(r+h - h/4),int(c + w/4):int(c+w - w/4)]
+
+    massCenterRow = grid[0] * intensities
+    massCenterCol = grid[1] * intensities
+
+    massCenterRow = np.sum(np.ndarray.flatten(massCenterRow), dtype=np.int64)
+    massCenterCol = np.sum(np.ndarray.flatten(massCenterCol), dtype=np.int64) #dtype has to be np.int64 to avoid overflow
+
+    totalIntensity = np.sum(np.ndarray.flatten(intensities))
+    massCenterRow = massCenterRow / totalIntensity
+    massCenterCol = massCenterCol / totalIntensity
+
+    if np.isnan(massCenterRow): massCenterRow = trackWindow[1]
+    if np.isnan(massCenterCol): massCenterCol = trackWindow[0]
 
     massCenterRow = int(massCenterRow)
     massCenterCol = int(massCenterCol)
 
 
-    pts = np.array([[massCenterCol-h/2, massCenterRow-w/2],
-                    [massCenterCol-h/2, massCenterRow+w/2],
-                    [massCenterCol+h/2, massCenterRow+w/2],
-                    [massCenterCol+h/2, massCenterRow-w/2]])
+    pts = np.array([[massCenterCol-w/2, massCenterRow-h/2],
+                    [massCenterCol-w/2, massCenterRow+h/2],
+                    [massCenterCol+w/2, massCenterRow+h/2],
+                    [massCenterCol+w/2, massCenterRow-h/2]])
 
-    return pts
+    trackWindow=(massCenterCol-w/2, massCenterRow-h/2,w,h)
+    return pts, trackWindow
 
 # for x, y in dst:
 # if rad^2 >= (x-center[0])^2 + (y - center[1])^2:
@@ -99,14 +127,15 @@ while True:
 
     if ret:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1 )
 
         # apply meanshift to get the new location
         # ret, track_window = cv2.CamShift(dst, track_window, term_crit)
 
         pts = 0
         for i in range(NUM_ITERAIONS):
-            pts = calculatePoints(track_window, 10, dst)
+            pts, track_window = calculatePoints(track_window, 10, dst)
+        print(track_window)
 
         # Draw it on image
         # pts = cv2.boxPoints(ret)
